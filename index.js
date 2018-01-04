29c6756d91b34916a7d251b0b6253772b5db56f1
Josh Gachnang <josh@servercobra.com>
2018-01-04 10:09:47 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+
+if (require.main === module) {
+  if (process.argv.length !== 4) {
+    console.log("Usage: createPresignedUrl <bucket> <filename>");
+    process.exit(1);
+  }
+
+  // This will load credentials automatically from whatever source you have configured.
+  const s3 = new AWS.S3();
+
+  let params = {
+    Bucket: process.argv[2],
+    Key: process.argv[3],
+    Expires: 315360000 // 10 years
+  };
+
+  // Create the signed URL and print it to stdout
+  let putUrl = s3.getSignedUrl("putObject", params);
+  let getUrl = s3.getSignedUrl("getObject", params);
+  console.log(
+    `You can upload to this URL like this:\n\n    curl "${putUrl}" --upload-file FILENAME\n`
+  );
+  console.log(
+    `You can then download the file like this:\n\n    curl -o FILENAME "${getUrl}"`
+  );
+}
